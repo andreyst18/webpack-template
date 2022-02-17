5d801a08a51aa8251296dc3645d51bf895c5a091
Andrey Strelin <andrejmax@yandex.ru>
2022-02-17 19:37:53 +0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import './assets/js/script';
+import './assets/styles/style.scss';
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: true
+    }),
+
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: path.resolve(__dirname, './src/pages'), to: path.resolve(__dirname, 'dist') },
+    //   ],
+    // }),
+  ],
+};
